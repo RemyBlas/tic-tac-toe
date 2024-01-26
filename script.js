f0069996bc5dd519db8ea60e146e2bbdd0f5c542
Leandro Blasco <lsblasco@hotmail.com>
2024-01-26 01:51:41 -0300

    Some CSS refined; Most functions wrappe in module "gameplayHandler"; Pending a way to wrap some loose global variants

--- script.js
+++ script.js
@@ -1,150 +1,150 @@
-// Toggle interactivity off if gameOver
-// Add a "next round" button to the pop-up that resets board
-// Add a global score keeping count of each player's score (games won)
-
-let gameBoard = [];
-
 function Player(name, value, symbol, score) {
-  // Maybe adding getScore, setScore functions?
-  return { name, value, symbol, score };
+  function getScore() {
+    return score;
+  }
+  function setScore() {
+    score++;
+  }
+  return { name, value, symbol, getScore, setScore };
 }
 
 const player1 = Player("Player 1", 1, "X", 0);
 const player2 = Player("Player 2", 2, "O", 0);
 
+let gameBoard = [];
 let activePlayer = player1;
-let gameOver;
 
 const table = document.querySelector(".gameboard");
-const nextRoundBtn = document.querySelector(".next_round");
-const modal = document.querySelector(".modal_container");
-
-resetBoard();
-
-function displayPlayers() {
-  const display = document.querySelector(".players_display");
-  display.textContent = `${activePlayer.name} turn: ${activePlayer.symbol}`;
-
-  const scoreLabel = document.querySelector(".score_label");
-  scoreLabel.textContent = `${player1.name}: ${player1.score} - ${player2.name}: ${player2.score}`;
-}
-
-displayPlayers();
-
 table.addEventListener("click", (e) => {
   const i = Number(e.target.id.replace("c", ""));
   if (gameBoard[i] === 0) {
+    gameplayHandler.playTurn(i);
+  }
+});
+
+const gameplayHandler = (function () {
+  let gameOver = false;
+  const modal = document.querySelector(".modal_container");
+  const nextRoundBtn = document.querySelector(".next_round");
+
+  function _displayPlayers() {
+    const display = document.querySelector(".players_display");
+    display.textContent = `${activePlayer.name} turn: ${activePlayer.symbol}`;
+  }
+
+  function _displayScore() {
+    const scoreLabel = document.querySelector(".score_label");
+    scoreLabel.textContent = `${player1.name}: ${player1.getScore()} - ${
+      player2.name
+    }: ${player2.getScore()}`;
+  }
+
+  function _alternatePlayer() {
+    activePlayer === player1
+      ? (activePlayer = player2)
+      : (activePlayer = player1);
+  }
+
+  function playTurn(i) {
     gameBoard[i] = activePlayer.value;
-    displaySymbols(i);
-    checkWin();
-    alternatePlayer();
-    displayPlayers();
+    _displaySymbols(i);
+    _checkWin();
+    _alternatePlayer();
+    _displayPlayers();
   }
-});
 
-nextRoundBtn.addEventListener("click", () => {
-  hideModal();
-  resetBoard();
-});
-
-// Can't do this this way because it hides the "next round" button. Maybe refactor.
-// modal.addEventListener("click", (e) => {
-//   const clickInside = document.querySelector(".modal").contains(e.target);
-//   if (!clickInside) {
-//     hideModal();
-//   }
-// });
-
-function resetBoard() {
-  gameOver = false;
-  gameBoard = [];
-  for (let i = 0; i < 9; i++) {
-    gameBoard.push(0);
+  function _checkWin() {
+    _checkRows();
+    _checkColumns();
+    _checkDiagonals();
+    if (gameOver) {
+      const msg = `${activePlayer.name} wins!`;
+      _showModal(msg);
+      activePlayer.setScore();
+    } else if (!gameBoard.includes(0)) {
+      const msg = "IT'S A TIE!";
+      _showModal(msg);
+    }
   }
-  for (let i = 0; i < 9; i++) {
+
+  function _checkRows() {
+    for (let i = 0; i < 9; i += 3) {
+      if (
+        gameBoard[i] > 0 &&
+        gameBoard[i] == gameBoard[i + 1] &&
+        gameBoard[i] == gameBoard[i + 2]
+      ) {
+        gameOver = true;
+      }
+    }
+  }
+
+  function _checkColumns() {
+    for (let i = 0; i < 3; i++) {
+      if (
+        gameBoard[i] > 0 &&
+        gameBoard[i] == gameBoard[i + 3] &&
+        gameBoard[i] == gameBoard[i + 6]
+      ) {
+        gameOver = true;
+      }
+    }
+  }
+
+  function _checkDiagonals() {
+    if (
+      (gameBoard[0] > 0 &&
+        gameBoard[0] == gameBoard[4] &&
+        gameBoard[0] == gameBoard[8]) ||
+      (gameBoard[2] > 0 &&
+        gameBoard[2] == gameBoard[4] &&
+        gameBoard[2] == gameBoard[6])
+    ) {
+      gameOver = true;
+    }
+  }
+
+  function _showModal(msg) {
+    const text = document.querySelector(".winner_label");
+
+    modal.classList.remove("hidden");
+    text.textContent = msg;
+  }
+
+  function _hideModal() {
+    modal.classList.add("hidden");
+  }
+
+  function _displaySymbols(i) {
     const cell = document.getElementById(`c${i}`);
-    cell.innerHTML = "";
+    const symbol = activePlayer.symbol;
+    cell.innerHTML = symbol;
+    cell.classList.add(`${symbol}`);
   }
-}
 
-function displaySymbols(i) {
-  const cell = document.getElementById(`c${i}`);
-  cell.innerHTML = activePlayer.symbol;
-}
+  nextRoundBtn.addEventListener("click", () => {
+    _hideModal();
+    _resetBoard();
+  });
 
-function alternatePlayer() {
-  activePlayer === player1
-    ? (activePlayer = player2)
-    : (activePlayer = player1);
-}
+  function _resetBoard() {
+    gameOver = false;
+    gameBoard = [];
 
-function checkWin() {
-  checkRows();
-  checkColumns();
-  checkDiagonals();
-  if (gameOver) {
-    const msg = `${activePlayer.name} wins!`;
-    showModal(msg);
-    activePlayer.score++;
-  } else if (!gameBoard.includes(0)) {
-    const msg = "It's a tie!";
-    showModal(msg);
-  }
-}
-
-function checkRows() {
-  for (let i = 0; i < 9; i += 3) {
-    if (
-      gameBoard[i] > 0 &&
-      gameBoard[i] == gameBoard[i + 1] &&
-      gameBoard[i] == gameBoard[i + 2]
-    ) {
-      gameOver = true;
+    for (let i = 0; i < 9; i++) {
+      gameBoard.push(0);
     }
-  }
-}
 
-function checkColumns() {
-  for (let i = 0; i < 3; i++) {
-    if (
-      gameBoard[i] > 0 &&
-      gameBoard[i] == gameBoard[i + 3] &&
-      gameBoard[i] == gameBoard[i + 6]
-    ) {
-      gameOver = true;
+    for (let i = 0; i < 9; i++) {
+      const cell = document.getElementById(`c${i}`);
+      cell.classList.remove("X", "O");
+      cell.innerHTML = "";
     }
+    _displayScore();
   }
-}
 
-function checkDiagonals() {
-  if (
-    (gameBoard[0] > 0 &&
-      gameBoard[0] == gameBoard[4] &&
-      gameBoard[0] == gameBoard[8]) ||
-    (gameBoard[2] > 0 &&
-      gameBoard[2] == gameBoard[4] &&
-      gameBoard[2] == gameBoard[6])
-  ) {
-    gameOver = true;
-  }
-}
+  _resetBoard();
+  _displayScore();
 
-function showModal(msg) {
-  const text = document.querySelector(".winner_label");
-
-  modal.classList.remove("hidden");
-  text.textContent = msg;
-}
-
-function hideModal() {
-  modal.classList.add("hidden");
-}
-
-// Revisar si hay 3 símbolos iguales en secuencia:
-//      Activar una función que finalice el juego y anuncie al jugador activo como ganador
-//      Si no hay secuencia ganadora, revisar si todos los cuadros del tablero están ocupados:
-//      Si es así, determinar fin de juego y empate
-
-// ¿Cómo determinar ganador?
-// ¿Chequear cada posible combinación? 3 filas, 3 columnas, 2 diagonales
-// Revisar cada una por si se repite el mismo símbolo y después determinar qué símbolo es para detectar quién ganó
+  return { playTurn };
+})();
